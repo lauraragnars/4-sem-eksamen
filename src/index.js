30b7f07cc92aa5fc0def78f5f96477052c905676
Laura Toft <laur507p@stud.kea.dk>
2021-05-24 12:31:01 +0200

    Small fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import { accessToken, spaceID } from "../config/contentful";
 
 const entryID = "7k6leOigNpIaVOJ3Nf9Mrb";
-
 const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries/${entryID}?access_token=${accessToken}`;
 
 window.addEventListener("load", start);
@@ -13,6 +12,8 @@ function start() {
 
   // show data at load
   loadJSON(link, showData);
+
+  animateText();
 }
 
 // loads data
@@ -26,4 +27,26 @@ function loadJSON(url, callback) {
 
 function showData(data) {
   console.log(data);
+}
+
+function animateText() {
+  const elem = document.querySelector(".splash-text");
+  const text = new Blotter.Text("DISTORTION Ø", {
+    family: "pilowlava-regular",
+    weight: 100,
+    size: 100,
+    fill: "white",
+  });
+
+  let material = new Blotter.RollingDistortMaterial();
+
+  material.uniforms.uSineDistortAmplitude.value = 0.04;
+
+  let blotter = new Blotter(material, {
+    texts: text,
+  });
+
+  let scope = blotter.forText(text);
+
+  scope.appendTo(elem);
 }
