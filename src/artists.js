0d40f4b103e9a61b02562a3927140d85e78c9287
Kristina Perlt Albech Andersen <kris79i3@stud.kea.dk>
2021-05-26 22:41:24 +0200

    artist dislay failed

--- src/artists.js
+++ src/artists.js
@@ -5,6 +5,8 @@ import { accessToken, spaceID } from "../config/contentful";
 const entryID = "6URuJ3VZ8sEaX4JN4z9DDk";
 const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries/${entryID}?access_token=${accessToken}`;
 
+let allArtists = [];
+
 window.addEventListener("load", start);
 
 function start() {
@@ -27,6 +29,25 @@ function loadJSON(url, callback) {
 
 function showData(data) {
   console.log(data);
-
   document.querySelector(".splash-text").textContent = data.fields.title;
+
+  showArtists();
+}
+
+function showArtists() {
+  let container = document.querySelector("#all-artists");
+  container.innerHTML = "";
+  let listTemplate = document.querySelector("template");
+
+  allArtists.forEach((artist) => {
+    if (filter == "lineup") {
+      let klon = listTemplate.cloneNode(true).textContent;
+      klon.querySelector("h2").textContent = artist.allArtists.individualArtist.artistName;
+      container.appendChild(klon);
+
+      // container.lastElementChild.addEventListener("click", () => {
+      //     location.href = "single.html?id=" + artist.;
+      // })
+    }
+  });
 }
