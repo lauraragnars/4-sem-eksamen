f11be6cf0bbd28af3b0f77299c4a11abe83ad1f6
Laura <laur507p@stud.kea.dk>
2021-05-26 23:41:20 +0200

    Artists hooked up and sorts by headliners

--- src/artists.js
+++ src/artists.js
@@ -3,9 +3,9 @@
 import { accessToken, spaceID } from "../config/contentful";
 
 const entryID = "6URuJ3VZ8sEaX4JN4z9DDk";
-const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries/${entryID}?access_token=${accessToken}`;
+// const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries?/${entryID}?access_token=${accessToken}`;
 
-let allArtists = [];
+const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries?access_token=${accessToken}&content_type=individualArtist`;
 
 window.addEventListener("load", start);
 
@@ -29,25 +29,45 @@ function loadJSON(url, callback) {
 
 function showData(data) {
   console.log(data);
-  document.querySelector(".splash-text").textContent = data.fields.title;
+  const list = document.querySelector("#all-artists");
+  const allArtists = data.items;
 
-  showArtists();
-}
-
-function showArtists() {
-  let container = document.querySelector("#all-artists");
-  container.innerHTML = "";
-  let listTemplate = document.querySelector("template");
+  // sort the artists, so the headliners are first
+  allArtists.sort(function (a, b) {
+    return b.fields.isHeadliner - a.fields.isHeadliner;
+  });
 
   allArtists.forEach((artist) => {
-    if (filter == "lineup") {
-      let klon = listTemplate.cloneNode(true).textContent;
-      klon.querySelector("h2").textContent = artist.allArtists.individualArtist.artistName;
-      container.appendChild(klon);
+    let li = document.createElement("li");
+    li.textContent = artist.fields.artistName;
 
-      // container.lastElementChild.addEventListener("click", () => {
-      //     location.href = "single.html?id=" + artist.;
-      // })
+    // Checks if artist is headliner
+    if (artist.fields.isHeadliner === true) {
+      li.classList.add("headliner");
     }
+    // add the <li> to the html list
+    list.append(li);
   });
 }
+
+// function showArtists(data) {
+//   console.log(data);
+//   // const allArtists = data.allArtists;
+//   const container = document.querySelector("#all-artists");
+//   // container.innerHTML = "";
+
+//   // let listTemplate = document.querySelector("template");
+
+//   // allArtists.forEach((artist) => {
+//   //   if (filter == "lineup") {
+//   //     let klon = listTemplate.cloneNode(true).textContent;
+//   //     klon.querySelector("h2").textContent =
+//   //       artist.allArtists.individualArtist.artistName;
+//   //     container.appendChild(klon);
+
+//   //     // container.lastElementChild.addEventListener("click", () => {
+//   //     //     location.href = "single.html?id=" + artist.;
+//   //     // })
+//   //   }
+//   // });
+// }
