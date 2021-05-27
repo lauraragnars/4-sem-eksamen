4c0f1cb5e100985e79edd0a986d4c5e5889aac23
Kristina Perlt Albech Andersen <kris79i3@stud.kea.dk>
2021-05-27 12:20:34 +0200

    setup tickets

---
+++ src/tickets.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import { accessToken, spaceID } from "../config/contentful";
+
+const entryID = "7k6leOigNpIaVOJ3Nf9Mrb";
+const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries/${entryID}?access_token=${accessToken}`;
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("start");
+
+  // show data at load
+  loadJSON(link, showData);
+}
+
+// loads data
+function loadJSON(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      callback(jsonData);
+    });
+}
+
+function showData(data) {
+  console.log(data);
+
+  document.querySelector(".about-text-header").textContent = data.fields.aboutHeader;
+  document.querySelector(".about-text").textContent = data.fields.aboutText;
+}
