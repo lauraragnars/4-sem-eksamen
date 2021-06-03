1888d7cb5470b9ff2582f37be4e71ebb385ffcef
Laura <laur507p@stud.kea.dk>
2021-06-03 20:26:23 +0200

    Stage playlists added

--- src/program.js
+++ src/program.js
@@ -2,7 +2,7 @@
 
 import { accessToken, spaceID } from "../config/contentful";
 
-const entryID = "9Tp2XfVYOtuB36CZHDiPi";
+const entryID = "12epLU2zbSRAPyPIZlOoGB";
 
 const link = `https://cdn.contentful.com/spaces/${spaceID}/environments/master/entries?access_token=${accessToken}&content_type=individualArtist`;
 
@@ -24,11 +24,18 @@ function loadJSON(url, callback) {
     });
 }
 
-// function showData(data) {
-//   console.log(data);
-// }
-
-// scener: live, urban, rave, Tekno tunnel, disco,
+const playlists = {
+  live:
+    "https://open.spotify.com/playlist/3Nhtd3UIA3ZOnbadvlB8eP?si=07aa078ef4eb4633",
+  disco:
+    "https://open.spotify.com/playlist/6b4H28FXufCiF0sENtL0iV?si=441a6a8cfc6f4ff6",
+  teknotunnel:
+    "https://open.spotify.com/playlist/1Zb53rJIv2HsbeYufPfDCR?si=6910ceb63d0f4dae",
+  urban:
+    "https://open.spotify.com/playlist/1o21gcE79dD0MyIglmhe1L?si=f5c966eefeec4227",
+  rave:
+    "https://open.spotify.com/playlist/7lsNafhgKHk144rOODp1Yn?si=14226af0c8fc4189",
+};
 
 function showData(data) {
   console.log(data);
@@ -40,7 +47,15 @@ function showData(data) {
 
   scener.forEach((scene) => {
     let clone = template.cloneNode(true).content;
-    clone.querySelector(".scene-title").textContent = scene + " scene";
+    clone.querySelector(".scene-title").textContent = scene + " stage";
+
+    // const stage = scene.toLowerCase()
+
+    if (scene !== "Tekno Tunnel") {
+      clone.querySelector(".playlist").href = playlists[scene.toLowerCase()];
+    } else {
+      clone.querySelector(".playlist").href = playlists.teknotunnel;
+    }
 
     data.items.forEach((artist) => {
       // first only select the artists that correspond to the right scene
